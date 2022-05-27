7a993384aa0e20e3e599c49b6984002cfc770cd9
theguy20phi <bradenwepierce@gmail.com>
2022-05-26 18:04:37 -0600

    Set up documentation

--- atum/include/atum/atum.hpp
+++ atum/include/atum/atum.hpp
@@ -1,1 +1,13 @@
+/**
+ * @file atum.hpp
+ * @author Braden Pierce (bradenwepierce@gmail.com)
+ * @brief This file includes the headers for the rest of
+ * the atum library. For use with the 15" and 18" ATUM8
+ * robots for the 2022 to 2023 VRC game Spin Up.
+ * @date 2022-05-26
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #pragma once
